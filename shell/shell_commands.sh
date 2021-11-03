8140f7ced05a0cf4fffd737dc4e1a9cf132dc5de
Puneetha B M <puneetha.bm14@gmail.com>
2021-11-03 21:19:49 +0000

    Update shell_commands.sh

--- shell/shell_commands.sh
+++ shell/shell_commands.sh
@@ -3,6 +3,9 @@
 echo "syntax on" >> ~/.vimrc
 ```
 
+# Hide warning for zsh
+export BASH_SILENCE_DEPRECATION_WARNING=1 
+
 # Color terminal
 export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
 export CLICOLOR=1
